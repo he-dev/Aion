27f7b493f727dd9b516a258cd3e13405550d05cc
ᎬᏁᎶᎥᏁᎬᎬᏒ <he-dev@users.noreply.github.com>
2025-08-03 17:02:44 +0200

    Fixes preset using the wrong variable for its path

--- Aion.Npc/src/Home/Program.cs
+++ Aion.Npc/src/Home/Program.cs
@@ -9,10 +9,10 @@ internal static class Program
     public static int Main(string[] args)
     {
         var messageOption = new Option<string>("--message") { IsRequired = false };
-        var sleepOption = new Option<int>("--sleep") { IsRequired = false };
+        var sleepSecondsOption = new Option<int>("--sleep-seconds") { IsRequired = false };
         var exitCodeOption = new Option<int>("--exit-code") { IsRequired = false };
 
-        var rootCommand = new RootCommand { messageOption, sleepOption, exitCodeOption };
+        var rootCommand = new RootCommand { messageOption, sleepSecondsOption, exitCodeOption };
         var commandLine = rootCommand.Parse(args);
 
         if (commandLine.GetValueForOption(messageOption) is { } message)
@@ -20,16 +20,19 @@ internal static class Program
             Console.WriteLine(message);
         }
 
-        if (commandLine.GetValueForOption(sleepOption) is var sleep and > 0)
+        if (commandLine.GetValueForOption(sleepSecondsOption) is var sleep and > 0)
         {
-            Console.WriteLine($"Working hard for {sleep} seconds...");
+            Console.WriteLine($"Waiting for {sleep} seconds...");
             Thread.Sleep(sleep * 1000);
         }
 
-        var exitCode = commandLine.GetValueForOption(exitCodeOption);
-        if (exitCode != 0)
+        if (commandLine.GetValueForOption(exitCodeOption) is var exitCode && exitCode != 0)
         {
-            Console.Error.WriteLine($"Oops! ExitCode: {exitCode}");
+            Console.Error.WriteLine($"Oops! ({exitCode})");
+        }
+        else
+        {
+            Console.WriteLine("Done!");
         }
 
         return exitCode;
